9390f5d6a9209a033618b7f90349d1aa67c72f1e
sekon-am <sekon.am@gmail.com>
2015-04-12 21:05:40 +0300

    pages filter & diagnostic length filter

--- www/js/ctrl.js
+++ www/js/ctrl.js
@@ -1,6 +1,16 @@
 'use strict';
 angular.module('sfp',['angularFileUpload'])
+.filter('range',function () {
+	return function (input,from,to){
+		if(Object.prototype.toString.call( input ) === '[object Array]'){
+			return input.slice(from-1,to-1);
+		}
+	}
+})
 .controller('DataCtrl',['$scope', '$http', '$upload', function($scope, $http, $upload) {
+	$scope.diagnosticLenLimit = 60;
+	$scope.pagesFrom = 1;
+	$scope.pagesTo = 50;
 	var page = 0;
 	function loadData(num) {
 		$http.get('api/index.php?action=data&from='+num).success(function(data){
@@ -13,9 +23,9 @@ angular.module('sfp',['angularFileUpload'])
 		});
 	}
 	function loadPages(callback) {
-		$http.get('api/index.php?action=pages').success(function(data){
+		$http.get('api/index.php?action=pages&pagesfrom='+$scope.pagesFrom+'&pagesto='+$scope.pagesTo).success(function(data){
 			$scope.pages = data;
-			callback();
+			if(typeof callback == 'function')callback();
 		});
 	}
 /*	$scope.pageleft = function () {
@@ -43,7 +53,7 @@ angular.module('sfp',['angularFileUpload'])
 					var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
 					console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
 				}).success(function(data, status, headers, config){
-					loadPages(function(){});
+					loadPages();
 				}).error(function(data){
 					console.log('Errod during uploading process... '+data);
 				});
