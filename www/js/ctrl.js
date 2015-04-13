b72e2867ed546c4a2586a7f830c61c2a06873ede
sekon-am <sekon.am@gmail.com>
2015-04-13 13:07:32 +0300

    new pages 1.1

--- www/js/ctrl.js
+++ www/js/ctrl.js
@@ -1,62 +1,53 @@
 'use strict';
 angular.module('sfp',['angularFileUpload'])
-.filter('range',function () {
-	return function (input,from,to){
-		if(Object.prototype.toString.call( input ) === '[object Array]'){
-			return input.slice(from-1,to-1);
-		}
-	}
-})
 .controller('DataCtrl',['$scope', '$http', '$upload', function($scope, $http, $upload) {
-	$scope.diagnosticLenLimit = 60;
-	$scope.pagesFrom = 1;
-	$scope.pages_amount = 1;
-	var page_num= 0;
+	var page_num;
+	
+	function loadPages(callback) {
+		$http.get('api/index.php?action=pages').success(function(data){
+			$scope.pages_amount = data.pages_amount;
+			if(typeof callback == 'function')callback();
+		});
+	}
 	function loadData(num) {
 		$http.get('api/index.php?action=data&from='+num).success(function(data){
 			$scope.fieldset = data;
 			page_num = num;
-			$scope.pages[num].clss = 'active';
+			for(var i=0; i<$scope.pages.length; i++){
+				$scope.pages[i].clss = ($scope.pages[i].num == page_num) ? 'active' : '';
+			}
 		});
 	}
 	function update_pages() {
 		$scope.pages = [];
 		for(var i=$scope.pagesFrom-1;i<$scope.pagesTo;i++){
-			$scope.pages[i] = {num: i, clss: ''};
+			$scope.pages.push({num: i, clss: (i == page_num) ? 'active' : ''});
 		}
 	}
-	function loadPages(callback) {
-		$http.get('api/index.php?action=pages').success(function(data){
-			$scope.pages_amount = data.pages_amount;
-			update_pages();
-			if(typeof callback == 'function')callback();
-		});
-	}
+
+	$scope.loaddata = loadData;
+	$scope.diagnosticLenLimit = 60;
+	$scope.pagesFrom = 1;
+	$scope.pages_amount = 1;
+	loadPages(function(){
+		$scope.pagesTo = Math.min($scope.pages_amount,10);
+		loadData(0);
+	});
+
 	$scope.$watch('pagesFrom',function(){
 		update_pages();
-		if($scope.pagesFrom-1>page_num){
-			loadData($scope.pagesFrom-1);
+		if($scope.pagesFrom-1 > page_num){
+			page_num = $scope.pagesFrom-1;
+			loadData(page_num);
 		}
 	});
 	$scope.$watch('pagesTo',function(){
 		update_pages();
-		if($scope.pagesTo-1<page_num){
-			loadData($scope.pagesTo-1);
+		if($scope.pagesTo-1 < page_num){
+			page_num = $scope.pagesTo-1;
+			loadData(page_num);
 		}
 	});
-/*	$scope.pageleft = function () {
-		console.log('11');
-		if(page>0){
-			loadData(page-1);
-		}
-	};
-	$scope.pageright = function () {
-		console.log('22');
-		if(page<$scope.pages.length-1){
-			loadData(page+1);
-		}
-	};*/
-	$scope.loaddata = loadData;
 	$scope.$watch('files',function(){
 		var files = $scope.files;
 		if(files && files.length){
@@ -75,9 +66,5 @@ angular.module('sfp',['angularFileUpload'])
 				});
 			}
 		}
-	});
-	loadPages(function(){
-		$scope.pagesTo = Math.min($scope.pages_amount,10);
-		loadData(0);
 	});
 }]);
